b8719165f8826fb3dbd47ba82ee569d4c96ea336
kritika.saxena <kritika.saxena@guavus.com>
2019-10-07 18:31:51 +0530

    Added pre chaos  data validation

--- chaostoolkit_nimble/tests/sample/platform/test_node_resiliency.py
+++ chaostoolkit_nimble/tests/sample/platform/test_node_resiliency.py
@@ -24,33 +24,6 @@ class TestNodeResiliency():
     def sample_actions(self):
         return SampleActions(self.job_alias)
 
-    # def test_2(self):
-    #     res = ambari_utils.all_services_running()
-    #     pass
-    #
-    # def test_1(self):
-    #     url = "http://192.168.134.140:8080/api/v1/clusters/testautomation-reflex-platform/request_schedules"
-    #     ambari_header = {"X-Requested-By": "ambari"}
-    #     auth = ("admin", "Sbe7UGkRBMF8")
-    #     payload = '[{"RequestSchedule":{"batch":[{"requests":[{"order_id":1,"type":"POST","uri":"\/api\/v1\/clusters\/testautomation-reflex-platform\/requests","RequestBodyInfo":{"RequestInfo":{"context":"HDFS Service Check (batch 1 of 3)","command":"HDFS_SERVICE_CHECK"},"Requests\/resource_filters":[{"service_name":"HDFS"}]}},{"order_id":2,"type":"POST","uri":"\/api\/v1\/clusters\/testautomation-reflex-platform\/requests","RequestBodyInfo":{"RequestInfo":{"context":"YARN Service Check (batch 2 of 3)","command":"YARN_SERVICE_CHECK"},"Requests\/resource_filters":[{"service_name":"YARN"}]}},{"order_id":3,"type":"POST","uri":"\/api\/v1\/clusters\/testautomation-reflex-platform\/requests","RequestBodyInfo":{"RequestInfo":{"context":"MapReduce Service Check (batch 3 of 3)","command":"MAPREDUCE2_SERVICE_CHECK"},"Requests\/resource_filters":[{"service_name":"MAPREDUCE2"}]}}]},{"batch_settings":{"batch_separation_in_seconds":1,"task_failure_tolerance":1}}]}}]'
-    #     payload = json.dumps(payload)
-    #     api_request_specification = ApiRequestSpecification(request_type=RequestType.POST, url=url, auth=auth,
-    #                                                         json=payload, headers=ambari_header, verify=False)
-    #     # api_request_specification.set_content_type(ContentType.FORM_URLENCODED)
-    #     api_request = ApiRequest()
-    #     request_schedule_response = api_request.execute_request(api_request_specification)
-    #     reponse = "bsajdjs"
-    #     try:
-    #         request_schedule_id = request_schedule_response.json()["resources"][0]["RequestSchedule"]["id"]
-    #         url = "http://192.168.134.140:8080/api/v1/clusters/testautomation-reflex-platform/request_schedules/%s" % request_schedule_id
-    #         api_request_specification = ApiRequestSpecification(request_type=RequestType.GET, url=url, auth=auth,
-    #                                                             headers=ambari_header, verify=False)
-    #         batch_request_response = api_request.execute_request(api_request_specification)
-    #         running_service_check_status = batch_request_response.json()["RequestSchedule"]["status"]
-    #     except KeyError:
-    #         pass
-    #     pass
-
     @pytest.fixture(scope="session")
     def hive_utils(self):
         if NodeManager.node_obj.kerberized is True:
@@ -74,6 +47,11 @@ class TestNodeResiliency():
         hadoop_cli_utils.remove(hdfs_path, recursive=True)
         hive_utils.drop_database(self.input_db)
 
+    def test_validate_pre_chaos(self, user_actions, sample_actions, send_data_to_hive):
+        # pylint: disable=unused-argument
+        DynamicSubstitutionUtils.add({"randynamic_db_name": self.input_db})
+        DynamicSubstitutionUtils.add({"randynamic_table_name": self.input_table})
+        user_actions.validate(sample_actions.validate_hive_to_hive, self.job_alias)
 
     def test_node_failover(self, send_data_to_hive):
         # pylint: disable=unused-argument
@@ -84,8 +62,8 @@ class TestNodeResiliency():
         # chaos_user_actions.run_experiment(exp_template_file=exp_template_file, context=context)
         chaos_user_actions.run_experiment(custom_exp_template_file=OPTIONS_DICT["experimentsPath"], context=context)
 
-    def test_validate(self, user_actions, sample_actions, send_data_to_hive):
+    def test_validate_post_chaos(self, user_actions, sample_actions, send_data_to_hive):
         # pylint: disable=unused-argument
         DynamicSubstitutionUtils.add({"randynamic_db_name": self.input_db})
         DynamicSubstitutionUtils.add({"randynamic_table_name": self.input_table})
-        user_actions.validate(sample_actions.validate_hive_to_hive, self.job_alias)
+        user_actions.validate(sample_actions.validate_hive_to_hive, self.job_alias, dataset_alias="dataset2")
